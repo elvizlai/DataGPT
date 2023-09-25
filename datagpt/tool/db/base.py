acc02b978a9785c6b01b05072691eeb7d786c2a2
elvizlai <sdrzlyz@gmail.com>
2023-09-26 00:00:46 +0800

    feat: using sqlalchemy to fetch schema info

--- datagpt/tool/db/base.py
+++ datagpt/tool/db/base.py
@@ -1,6 +1,10 @@
 from abc import ABCMeta, abstractmethod
 from typing import Iterable, List, Optional
 
+from sqlalchemy import create_engine, inspect, text
+
+from datagpt.log import logger
+
 
 class TableInfo:
     name: str
@@ -60,3 +64,71 @@ class RDBMS(metaclass=ABCMeta):
     @abstractmethod
     def run(self, command: str) -> List:
         pass
+
+
+class Engine(RDBMS):
+    def __init__(self, uri: str):
+        if uri.startswith("postgresql"):
+            if uri.startswith("postgresql://"):
+                uri = uri.replace("postgresql://", "postgresql+psycopg://")
+            self._type = "PostgreSQL"
+        elif uri.startswith("mysql") or uri.startswith("mariadb"):
+            self._type = "MySQL"
+        else:
+            self._type = "Unknown"
+
+        self._uri = uri
+        self._engine = create_engine(uri)
+        self._inspector = inspect(self._engine)
+
+    def get_type(self) -> str:
+        return self._type
+
+    def get_tables(self) -> Iterable[TableInfo]:
+        table_names = self._inspector.get_table_names()
+
+        return [
+            TableInfo(name, self._inspector.get_table_comment(name))
+            for name in table_names
+        ]
+
+    def get_fields(
+        self, table_name: str, schema: str = "public"
+    ) -> Iterable[FieldInfo]:
+        if self._type != "PostgreSQL":
+            schema = None
+
+        field_records = self._inspector.get_columns(table_name, schema)
+
+        table_fields: List[FieldInfo] = []
+
+        for field in field_records:
+            f = FieldInfo(
+                field["name"],
+                field["type"],
+                None,
+                field["default"],
+                field["nullable"],
+                field["comment"],
+            )
+            if hasattr(field["type"], "__visit_name__"):
+                f.col_type = field["type"].__visit_name__
+
+            if hasattr(field["type"], "length"):
+                f.col_length = field["type"].length
+
+            table_fields.append(f)
+
+        return table_fields
+
+    def run(self, command: str) -> List:
+        try:
+            with self._engine.connect() as conn:
+                result = conn.execute(text(command))
+                rows = result.fetchall()
+                cols = result.keys()
+                rows.insert(0, tuple([col for col in cols]))
+                return rows
+        except Exception as e:
+            logger.error("run sql error: " + str(e))
+            return "SqlError"
